f7053e27ace60007f45ae85f1e713297f3433874
sunwoo <sswsjw1234@gmail.com>
2025-09-04 00:19:21 +0900

    initial commit

---
+++ src/main/java/backend/mydays/MydaysApplication.java
@@ -0,0 +1,13 @@
+package backend.mydays;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MydaysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MydaysApplication.class, args);
+	}
+
+}
---
+++ src/test/java/backend/mydays/MydaysApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.mydays;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MydaysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
