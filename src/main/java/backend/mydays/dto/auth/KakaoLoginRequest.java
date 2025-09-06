4391df2c99c0225a1bf0d320b88e4f2a3d9abb8d
Ahnsukjoo <bos98@naver.com>
2025-09-07 06:30:50 +0900

    feat(auth): Update KakaoLoginRequest DTO to match client

--- src/main/java/backend/mydays/dto/auth/KakaoLoginRequest.java
+++ src/main/java/backend/mydays/dto/auth/KakaoLoginRequest.java
@@ -1,10 +1,12 @@
 package backend.mydays.dto.auth;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
 public class KakaoLoginRequest {
-    private String kakaoAccessToken;
+    private String accessToken;
+    private String refreshToken; // This will be ignored by the backend
 }
